47acfb0aff48dfa477dbe8f92429a4241284c840
fRankEnstein1-1-1-1 <frankensteinmenon@gmail.com>
2026-05-15 00:10:14 +0530

    vercel.json

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const API = axios.create({
     baseURL: 'https://mockingbird-joc8.onrender.com/api'
 });
-
+// http://localhost:5000
+//https://mockingbird-joc8.onrender.com
 // Add token to every request
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
